249a9ad14d72c23438e5dc5b852178f3728437de
fede97inno <innofede97@gmail.com>
2024-01-05 01:22:21 +0100

    Remove Development
    Development of the remove function, refactoring of the code.

--- linked_list.c
+++ linked_list.c
@@ -1,34 +1,30 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define AIV_LIST(x) &(x.list_item)      //MACRO to define something in the code
+#define LINKED_LIST(x) &(x.list_item)      //MACRO to define something in the code
 
-struct aiv_list_item                    //generic list pointer
+typedef struct linked_list_item                    //generic list pointer
 {
-    struct aiv_list_item* next;         //pointer at next item of the list
+    struct linked_list_item* next;         //pointer at next item of the list
     unsigned int count;    
-};
+}linked_list_item;
 
-struct aiv_int_item                     //list of ints
+typedef struct linked_int_item                     //list of ints
 {
-    struct aiv_list_item list_item;
+    linked_list_item list_item;
     int value;                          //int element of the list
-};
+}linked_int_item;
 
-struct aiv_c_sharp_list
-{
-    struct aiv_list_item* head;
-    unsigned int counter;
-};
-
-struct aiv_list_item* aiv_list_get_tail(struct aiv_list_item* head)
+linked_list_item* linked_list_get_tail(linked_list_item* head)
 {
     if (!head)                          //if it hasn't head can't have tail
     {
         return NULL;
     }
 
-    struct aiv_list_item* current_item = head;
-    struct aiv_list_item* last_item = head;
+    linked_list_item* current_item = head;
+    linked_list_item* last_item = head;
 
     while(current_item)
     {
@@ -39,9 +35,9 @@ struct aiv_list_item* aiv_list_get_tail(struct aiv_list_item* head)
     return last_item;
 }
 
-struct aiv_list_item* aiv_list_append(struct aiv_list_item** head, struct aiv_list_item* item)
+linked_list_item* linked_list_append(linked_list_item** head, linked_list_item* item)
 {
-    struct aiv_list_item* tail = aiv_list_get_tail(*head);
+    struct linked_list_item* tail = linked_list_get_tail(*head);
 
     if (!tail)
     {
@@ -58,30 +54,19 @@ struct aiv_list_item* aiv_list_append(struct aiv_list_item** head, struct aiv_li
     return item;
 }
 
-unsigned int aiv_list_lenght(struct aiv_list_item* head)
+unsigned int linked_list_lenght(linked_list_item* head)
 {
-    // unsigned int counter = 0;
-    // struct aiv_list_item* current_item = head;
-
-    // while(current_item)
-    // {
-    //     counter++;
-    //     current_item = current_item->next;
-
-    //     return counter;
-    // }
-
     return head->count;
 }
 
-struct aiv_list_item* aiv_list_pop(struct aiv_list_item** head)
+linked_list_item* linked_list_pop(linked_list_item** head)
 {
     if (!(*head))
     {
         return NULL;
     }
     
-    struct aiv_list_item* current_head = *head;
+    struct linked_list_item* current_head = *head;
     const unsigned int current_count = (*head)->count;
     *head = (*head) -> next;                                        //new head
 
@@ -94,31 +79,98 @@ struct aiv_list_item* aiv_list_pop(struct aiv_list_item** head)
     return current_head;
 }
 
-struct aiv_list_item* aiv_remove_item_by_addres(struct aiv_list_item** head, struct aiv_list_item* address)
+linked_list_item* linked_list_remove_item(linked_list_item** head, linked_list_item* item_to_remove)
 {
     if (!(*head))
     {
         return NULL;
     }
 
-    struct aiv_list_item* current_head = *head;
+    linked_list_item* current_item = *head;
 
+    if (current_item == item_to_remove)
+    {
+        return linked_list_pop(head);
+    }
+
+    linked_list_item* next_item = current_item->next;
+
+    while (next_item != item_to_remove)
+    {
+        current_item = current_item->next;
+        next_item = next_item->next;
+    }
+
+    current_item->next = next_item->next;
+    next_item->next =NULL;
+
+    return next_item;
+}
+
+void linked_list_print(linked_list_item* head)
+{
+    printf("--------------------\n");
+    struct linked_int_item* current_item = (struct linked_int_item*)head;
+    
+    while(current_item)
+    {
+        printf("%d\n", current_item->value);
+        current_item = ((struct linked_int_item*)current_item->list_item.next);
+    }
+}
+
+void linked_list_reverse(linked_list_item** head)
+{
+    linked_list_item* current_item = *head;
+    current_item->next = NULL;
+    linked_list_item* next_item = (*head)->next;
+    printf("hey");
+
+    while(current_item)
+    {
+        next_item->next = current_item;
+        current_item = current_item->next;
+        printf("hey");
+    }
+
+    //head = *next_item;
 }
 
 int main(int argc, char** argv)
 {
-    struct aiv_list_item* head = NULL;      //exemple : value = 0 address 0x4000
+    linked_list_item* head = NULL;      
+    
+    linked_int_item int_item0;
+    int_item0.value = 100;
+    linked_list_append(&head,  LINKED_LIST(int_item0));   
 
-    struct aiv_int_item int_item;
-    int_item.value = 100;
-    aiv_list_append(&head, (struct aiv_list_item*)&int_item);   //casts don't change OPCODES, it just shut up the warnings, C is a waekly typed 
-                                                                //language, you can cast if the first item address correspond to the actual address
-    struct aiv_int_item int_item2;
-    int_item.value = 101;
-    aiv_list_append(&head, &(int_item2.list_item));             //you can also do this
+    linked_int_item int_item1;
+    int_item1.value = 101;
+    linked_list_append(&head, LINKED_LIST(int_item1));             
 
-    struct aiv_int_item int_item3;
-    int_item.value = 102;
-    aiv_list_append(&head, AIV_LIST(int_item));                 //you can also do this
+    linked_int_item int_item2;
+    int_item2.value = 102;
+    linked_list_append(&head, LINKED_LIST(int_item2));                 
+
+    linked_int_item int_item3;
+    int_item3.value = 103;
+    linked_list_append(&head, LINKED_LIST(int_item3));                 
+
+    linked_int_item int_item4;
+    int_item4.value = 104;
+    linked_list_append(&head, LINKED_LIST(int_item4));                 
+    
+    linked_list_print(head);
+
+    //linked_list_pop(&head);
+
+    //linked_list_remove_item(&head, LINKED_LIST(int_item4));
+
+    //linked_list_print(head);
+
+    linked_list_reverse(&head);
+    
+    linked_list_print(head);
+
     return 0;
 }
